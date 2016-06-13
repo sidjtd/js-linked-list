881768e0ee22ce8defe3cc7bc0d44c26854ccb5f
Sidney <user@SGD-MBP.local>
2016-06-13 11:34:40 -1000

    Got first 2 completed

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,53 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
+ var head = null;
+ var tail = null;
+
 function linkedListGenerator(){
 
-}
+  function getHead(){
+    return head;
+  }
+
+  function getTail(){
+    return tail;
+  }
+
+  function add(valuex){
+   var node = {
+    value : valuex,
+    next : null
+    };
+
+    if(head===null&&tail===null){
+      tail = node;
+      head = node;
+      }
+    else if(head!==null){
+      tail = node;
+    }
+    else{
+
+    }
+    return node;
+  }
+
+  function get(num){
+  }
+  function remove(num){
+  }
+  function insert(value,num){
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
+  };
+
+}//Dont mess with this brace
+var movies = linkedListGenerator();
